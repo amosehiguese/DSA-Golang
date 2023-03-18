4c15de716c3fbbf5c3f67a20bf03a4bbf3a5ffb4
amosehiguese <amosehiguese@gmail.com>
2023-03-18 11:01:29 +0100

    Add rectangle.go

---
+++ 02-conditionals-loops/perimeters/rectangle.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+type Rectangle struct {
+	length, width float64
+}
+
+func (r *Rectangle) Perimeter() {
+	perimeter := 2 * (r.length + r.width)
+	fmt.Printf("Area of the Rectangle is: %.2f\n", perimeter)
+}
