74217c8613535f088442f3fb4016346664091226
amosehiguese <amosehiguese@gmail.com>
2023-07-30 09:41:36 +0100

    Modify product-sum
    Signed-off-by: amosehiguese <amosehiguese@gmail.com>

--- 02-conditionals-loops/subtract-product-and-sum.go
+++ 02-conditionals-loops/subtract-product-and-sum.go
@@ -1,12 +1,14 @@
-func subtractProductAndSum(n int) int {
-    var product int = 1
-    var sum = 0
-    for n > 0 {
-        rem := n % 10
-        product *= rem
-        sum += rem 
-        n /= 10
-    }
-    result := product - sum
-    return result
+package sub
+
+func SubtractProductAndSum(n int) int {
+	var product int = 1
+	var sum = 0
+	for n > 0 {
+		rem := n % 10
+		product *= rem
+		sum += rem
+		n /= 10
+	}
+	result := product - sum
+	return result
 }
